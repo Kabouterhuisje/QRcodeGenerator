d5ba931ff863bd7cb24824e183a8b67b0fc72c0c
Dennis Tijbosch <d.tijbosch@yahoo.com>
2016-01-05 09:00:14 +0100

    Basics
    Just the basics of the generator

---
+++ QRcodeCreator/QRcodeCreator/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace QRcodeCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbQR = new System.Windows.Forms.PictureBox();
+            this.lbURL = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbQR)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbQR
+            // 
+            this.pbQR.Location = new System.Drawing.Point(12, 12);
+            this.pbQR.Name = "pbQR";
+            this.pbQR.Size = new System.Drawing.Size(450, 401);
+            this.pbQR.TabIndex = 0;
+            this.pbQR.TabStop = false;
+            // 
+            // lbURL
+            // 
+            this.lbURL.AutoSize = true;
+            this.lbURL.Location = new System.Drawing.Point(12, 432);
+            this.lbURL.Name = "lbURL";
+            this.lbURL.Size = new System.Drawing.Size(32, 13);
+            this.lbURL.TabIndex = 1;
+            this.lbURL.Text = "URL:";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(50, 429);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(412, 20);
+            this.txtURL.TabIndex = 2;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(15, 465);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(447, 23);
+            this.btnCreate.TabIndex = 3;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 494);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(447, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 523);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(218, 23);
+            this.btnOpen.TabIndex = 5;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(236, 523);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(223, 23);
+            this.btnConvert.TabIndex = 6;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 561);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.lbURL);
+            this.Controls.Add(this.pbQR);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "QR Code Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.pbQR)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbQR;
+        private System.Windows.Forms.Label lbURL;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ QRcodeCreator/QRcodeCreator/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MessagingToolkit.QRCode.Codec;
+using MessagingToolkit.QRCode.Codec.Data;
+
+namespace QRcodeCreator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Create QR Code
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            string URL = txtURL.Text;
+            QRCodeEncoder encoder = new QRCodeEncoder();
+            Bitmap qrcode = encoder.Encode(URL);
+            pbQR.Image = (Image) qrcode;
+        }
+
+        // Save QR Code
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog s = new SaveFileDialog {Filter = @"PNG|*.png|JPEG|*.jpeg|BMP|*.bmp|GIF|*.gif"};
+            if (s.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                pbQR.Image.Save(s.FileName);
+            }
+        }
+
+        // Open QR Code
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog o = new OpenFileDialog();
+            if (o.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                pbQR.ImageLocation = o.FileName;
+            }
+        }
+
+        // Convert QR Code back to URL
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            QRCodeDecoder decoder = new QRCodeDecoder();
+            MessageBox.Show(decoder.decode(new QRCodeBitmapImage(pbQR.Image as Bitmap)));
+        }
+    }
+}
